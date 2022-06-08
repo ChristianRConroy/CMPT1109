fe40ea192f9a8f49a3cfe5ad45ae4a4e11de13f0
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-08 13:20:47 -0700

    converted is_leap() to bool

--- Labs/Lab 4.cpp
+++ Labs/Lab 4.cpp
@@ -3,18 +3,12 @@
 using namespace std;
 
 
-void is_leap(int year){
+bool is_leap(int year){
 
-int rem;
-
-int rem2;
-
-int rem3;
+int rem, rem2, rem3;
 
 rem = year % 4;
-
 rem2 = year % 100;
-
 rem3 = year % 400;
 
 if(rem == 0)
@@ -25,22 +19,32 @@ if(rem == 0)
      if(rem3 ==0)
      {
         cout << "leap year" << endl;
+        return true;
      }
      else
+        {
         cout <<"not a leap year" << endl;
+        return false;
+        }
  }
  else
+    {
     cout <<"leap year"<< endl;
+    return true;
+    }
 }
 else
+    {
     cout <<"common year" << endl;
-
+    return false;
+    }
 
 }
+
 
 int main(){
 
-is_leap(2000);
+is_leap(2003);
 
 return 0;
 
