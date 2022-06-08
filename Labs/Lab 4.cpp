0279d1530a642940d47aa3e145840ad0c1444e9e
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-08 14:13:40 -0700

    Added multidimensional arrays
    code is not printing. number of days per month function was added

--- Labs/Lab 4.cpp
+++ Labs/Lab 4.cpp
@@ -37,11 +37,67 @@ else
 
 }
 
+int num_days_per_month(int month)
+{
+
+switch (month) {
+
+    case 1:
+    return 30;
+        break;
+
+    default:
+        return 31;
+
+}
+
+}
 
 int main(){
+const int ROWS = 5;
+
+const int COLUMNS = 7;
+
 
 is_leap(1900);
 
 return 0;
+
+int month[ROWS][COLUMNS];
+
+int day = 1;
+
+num_days_per_month(1);
+
+
+for (int i = 0; i < ROWS; i++) //for rows
+{
+    for(int j = 0; i < COLUMNS && day <= num_days_per_month(1); j++)
+    {
+        month[i][j] = day++;
+    }
+}
+
+
+
+int counter = 1;
+//to print the thing
+for (int i = 0; i < ROWS; i++) //for rows
+{
+    for(int j = 0; i < COLUMNS && day <= num_days_per_month(1); j++)
+    {
+        cout << month[i][j] << " ";
+        counter++;
+
+    }
+    cout << endl;
+}
+
+// jan 2004 starting from sunday
+//is_leap(2004);
+
+
+
+
 
 }
