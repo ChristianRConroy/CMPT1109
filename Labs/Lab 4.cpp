6a6e2ad15f41a4db95c3edb20ad3ef7e9681a9ab
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-08 12:44:15 -0700

    Added inputs

--- Labs/Lab 4.cpp
+++ Labs/Lab 4.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 int main(){
 
+int year;
 
+int janFirst;
 
+string outputName;
+
+cout << "please enter your calender year" << endl;
+cin >> year;
+
+cout << "please enter which day the first of january falls on" << endl;
+cin >> janFirst;
+
+cout << "please enter your output file name" << endl;
+cin >> outputName;
 
 
     return 0;
