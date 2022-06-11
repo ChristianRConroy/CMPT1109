e02c8d650393c36350e47bcc1a9345bcdf4cd028
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-11 12:19:21 -0700

    added ProdCodeType and MainMenu
    prodcodetype effectively finds the array size for the rest of the program through an input

--- Assignments/Assignment_1.cpp
+++ Assignments/Assignment_1.cpp
@@ -1,25 +1,90 @@
 #include <iostream>
 
 using namespace std;
+//adds a nice menu to start with
+void mainMenu(){
+
+    cout << "check digit calculator" << endl;
+    cout << "----------------" << endl;
+    cout << "1. EAN-8" << endl;
+    cout << "2. UPC-A" << endl;
+    cout << "3. EAN-13" << endl;
+    cout << "4. EAN-14" << endl;
+    cout << "5. SSCC" << endl;
+    cout << "----------------" << endl;
+
+}
+
+//will effectively return the size of the array we will be using
+int prodCodeType() {
+
+    int productType, size = 0;
+
+    cout << "chose your product code type:";
+
+    cin >> productType;
+
+    switch (productType) {
+
+            case 1:
+            {
+                size = 8;
+                return size;
+            }
+                break;
+
+            case 2:
+            {
+                size = 12;
+                return size;
+            }
+                break;
+
+            case 3:
+            {
+                size = 13;
+                return size;
+            }
+                break;
+            
+            case 4:
+            {
+                size = 14;
+                return size;
+            }
+                break;
+            case 5:
+            {
+                size = 18;
+                return size;
+            }
+                break;
+            default:
+                {
+                cout <<"invalid entry" << endl;
+                return 0;
+                }
+    }
+}
 
 //adds odd elements of array together
 //this is right now built for a full array, not n-1 inputs, same with addevens
 int addOdds(int arr[], int size){
 
-int j, sum = 0;
+    int j, sum = 0;
 
-for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the array
-{
-    if(j < (size - 1)) //makes sure it doesnt add more numbers than are elements of the array
+    for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the array
     {
-        sum += arr[j]; //adds elements
-        j = j + 2;     //ensures only "odd" elements are added
+        if(j < (size - 1)) //makes sure it doesnt add more numbers than are elements of the array
+        {
+            sum += arr[j]; //adds elements
+            j = j + 2;     //ensures only "odd" elements are added
+        }
     }
-}
 
-    sum = sum * 3; //this multiplies the ans by 3
+        sum = sum * 3; //this multiplies the ans by 3
 
-return sum; //returns the sum
+            return sum; //returns the sum
 
 
 }
@@ -27,19 +92,19 @@ return sum; //returns the sum
 //adds even elements of array together
 int addEvens(int arr[], int size){
 
-int j = 1;
-int sum = 0;
+    int j = 1;
+    int sum = 0;
 
-for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the array
-{
-    if(j < size -1) //makes sure it doesnt add more numbers than are elements of the array
+    for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the array
     {
-        sum += arr[j]; //adds elements
-        j = j + 2;     //ensures only "even" elements are added  
+        if(j < size -1) //makes sure it doesnt add more numbers than are elements of the array
+        {
+            sum += arr[j]; //adds elements
+            j = j + 2;     //ensures only "even" elements are added  
+        }
     }
-}
 
-return sum; //returns the sum
+        return sum; //returns the sum
 
 
 }
@@ -48,11 +113,11 @@ return sum; //returns the sum
 int addEvens_Odds(int arr[], int size)
 {
 
-int sum = 0; 
+    int sum = 0; 
 
-sum = addEvens(arr,size) + addOdds(arr, size); //adds evens and odds together
+    sum = addEvens(arr,size) + addOdds(arr, size); //adds evens and odds together
 
-return sum;
+        return sum;
 
 }
 
@@ -60,34 +125,35 @@ return sum;
 int findCheck(int arr[], int size)
 {
 
-int ans, sum, alter = 0; //just to be certain no garbage is within variables
+    int ans, sum, alter = 0; //just to be certain no garbage is within variables
 
-sum = addEvens_Odds(arr, size); 
+    sum = addEvens_Odds(arr, size); 
 
-alter = sum; //gives us another var to work with
+    alter = sum; //gives us another var to work with
 
-while(alter % 10 != 0) //adds to the new alter variable until its the next tenth
-    {
-        alter++;
-    }
+    while(alter % 10 != 0) //adds to the new alter variable until its the next tenth
+        {
+            alter++;
+        }
 
-ans = alter - sum; 
+    ans = alter - sum; 
 
-return ans;
+            return ans;
 
 }
 
+//this displays the full digits of the barcode with the check digit
 void displayBarCode(int arr[], int size)
 {
 
-for(int i = 0; i < (size - 1); i++) //should print out array except for last digit
-{
+    for(int i = 0; i < (size - 1); i++) //should print out array except for last digit
+    {
 
-cout << arr[i];
+    cout << arr[i];
 
-}
+    }
 
-cout << findCheck(arr, size); //prints out last digits
+    cout << findCheck(arr, size); //prints out last digits
 
 }
 
@@ -95,9 +161,36 @@ cout << findCheck(arr, size); //prints out last digits
 int main(){
 
 
-const int CODE = 12;
+
+
+cout << "check digit calculator" << endl;
+cout << "----------------" << endl;
+cout << "1. EAN-8" << endl;
+cout << "2. UPC-A" << endl;
+cout << "3. EAN-13" << endl;
+cout << "4. EAN-14" << endl;
+cout << "5. SSCC" << endl;
+cout << "----------------" << endl;
+
+
+
+
+cout << prodCodeType();
+
+
+//cout << endl;
+
+//cout << "enter the first 11 digits of your product code:";
+
+
+
+
+
+
+
+//const int CODE = 12;
 //                   0  1  2  3  4  5  6  7  8  9  10 11
-int barCode[CODE] = {2, 3, 1, 3, 9, 0, 8, 5, 3, 7, 7, 9};
+//int barCode[CODE] = {2, 3, 1, 3, 9, 0, 8, 5, 3, 7, 7, 9};
 
 //cout << "odds" << endl;
 //addOdds(barCode, CODE);
