23adae5cbdc5324be43a50e1fc04c141caa7c3c6
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-11 11:46:18 -0700

    Added FindCheck function and fixed addevens_odds
    now the addodds function also multiplies its ans by 3. findcheck now finds the proper check digit through a while loop. addevens_odd has been simplified

--- Assignments/Assignment_1.cpp
+++ Assignments/Assignment_1.cpp
@@ -2,12 +2,11 @@
 
 using namespace std;
 
-//adds odd numbers together
+//adds odd elements of array together
+//this is right now built for a full array, not n-1 inputs, same with addevens
 int addOdds(int arr[], int size){
 
-int j = 0;
-
-int sum = 0;
+int j, sum = 0;
 
 for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the array
 {
@@ -18,6 +17,7 @@ for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the arra
     }
 }
 
+sum = sum * 3; //this multiplies the ans by 3
 cout << sum << endl; //just for testing right now
 
 return sum; //returns the sum
@@ -25,10 +25,10 @@ return sum; //returns the sum
 
 }
 
+//adds even elements of array together
 int addEvens(int arr[], int size){
 
 int j = 1;
-
 int sum = 0;
 
 for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the array
@@ -36,11 +36,7 @@ for(int i = 0; i < (size - 1); i++) //runs as many times as the size of the arra
     if(j < size -1) //makes sure it doesnt add more numbers than are elements of the array
     {
         sum += arr[j]; //adds elements
-        j = j + 2;     //ensures only "even" elements are added
-        
-       
-
-        
+        j = j + 2;     //ensures only "even" elements are added  
     }
 }
 
@@ -51,21 +47,40 @@ return sum; //returns the sum
 
 }
 
-
+//multiplies odds by three and adds to evens
 int addEvens_Odds(int arr[], int size)
 {
 
-int sum = 0;
+int sum = 0; 
 
-sum = addEvens(arr,size) + addOdds(arr, size);
-
-sum = sum * 3;
-
-cout << sum << endl;
+sum = addEvens(arr,size) + addOdds(arr, size); //adds evens and odds together
 
 return sum;
 
 }
+
+//finds the check digit through finding the diff 
+int findCheck(int arr[], int size)
+{
+
+int ans, sum, alter = 0; //just to be certain no garbage is within variables
+
+sum = addEvens_Odds(arr, size); 
+
+alter = sum; //gives us another var to work with
+
+while(alter % 10 != 0) //adds to the new alter variable until its the next tenth
+    {
+        alter++;
+    }
+
+ans = alter - sum; 
+
+return ans;
+
+}
+
+
 
 
 int main(){
@@ -75,11 +90,11 @@ const int CODE = 12;
 //                   0  1  2  3  4  5  6  7  8  9  10 11
 int barCode[CODE] = {2, 3, 1, 3, 9, 0, 8, 5, 3, 7, 7, 9};
 
-cout << "odds" << endl;
-addOdds(barCode, CODE);
-cout << "evens" << endl;
-addEvens(barCode, CODE);
-cout << "both added" << endl;
+//cout << "odds" << endl;
+//addOdds(barCode, CODE);
+//cout << "evens" << endl;
+//addEvens(barCode, CODE);
+//cout << "both added" << endl;
 addEvens_Odds(barCode, CODE);
 
 
