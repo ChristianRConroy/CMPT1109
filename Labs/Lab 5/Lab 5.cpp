6a7d933f35bcd5e5329d031a76ecbf083a6929e1
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-29 13:31:25 -0700

    now reads full text file

--- Labs/Lab 5/Lab 5.cpp
+++ Labs/Lab 5/Lab 5.cpp
@@ -9,42 +9,25 @@ int main(){
 
 ifstream file;
 
-file.open("answers.txt");
+char partAnswer[100];
 
-if(file.fail())
-{
-cout << "something went wrong" << endl;
+int counter = 0;
+
+file.get(partAnswer[counter++]); //this goes to the next character and .get reads from the file
+
+while(!file.eof()){ //this will allow us to read the whole line
+
+    if(partAnswer[counter] == '\n') //this will give us our answer
+    {   
+    cout << partAnswer;
+    counter = 0;
+    }
+   else
+   file.get(partAnswer[counter++]);
+
+
 
 }
-else
-
-cout << "all good" << endl;
-
-while(true){
-
-cout << "plz, enter your next question" << endl;
-
-
-string question;
-
-cin >> question;
-
-string answer;
-
-if (file >> answer)
-cout << answer << endl;
-
-else
-file.close();
-file.open("answers.txt");
-
-file >> answer;
-cout << answer << endl;
-
-}
-
-
-
 
 
 return 0;    
