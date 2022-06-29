fd12c8cc2426b606cd4d89d7e2161bb2f388b9aa
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-29 12:58:55 -0700

    start of file
    moving files around, not running

--- Labs/Lab 5/Lab 5.cpp
+++ Labs/Lab 5/Lab 5.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
+
+
 
 int main(){
 
+ifstream file;
+
+file.open("answers.txt");
+
+if(file.fail())
+{
+cout << "something went wrong" << endl;
+
+}
+else
+
+cout << "all good" << endl;
+
+while(true){
+
+cout << "plz, enter your next question" << endl;
+
+
+string question;
+
+cin >> question;
+
+string answer;
+
+if (file >> answer)
+cout << answer << endl;
+
+else
+file.close();
+file.open("answers.txt");
+
+file >> answer;
+cout << answer << endl;
+
+}
 
 
 
