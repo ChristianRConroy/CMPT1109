b7dd352b4f6d1762f0de0b98b06a8734e8637dde
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-06-29 12:39:26 -0700

    work

--- InClass/InClass.cpp
+++ InClass/InClass.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+
+int main()
+{
+
+	int x;
+	//cin >> x;
+	//cout << "sadasd" << x;
+
+
+
+
+	ifstream inStream;
+
+	inStream.open("input.txt");
+
+	if (inStream.fail()) {  //fail is boolean function that returns true or false depending if the 
+							//file was opened or not
+		cout << "something went wrong" << endl;
+		return -1;
+	
+	}
+
+	inStream >> x;
+
+	inStream.close(); //will close the open file because because
+
+	//cout << x;
+
+
+	ofstream outStream;
+
+	outStream.open("output.txt");
+
+	outStream << x;
+
+
+	return -1;
+
+
+
+}
