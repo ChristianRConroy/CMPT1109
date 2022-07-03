e22ea467962b43db6a7f2bfc356a1cabc7ca9349
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-07-03 15:49:20 -0700

    The input loop now works
    there is still garbage in the output but it takes an input consistently now

--- Labs/Lab 5/Lab 5.cpp
+++ Labs/Lab 5/Lab 5.cpp
@@ -19,13 +19,9 @@ int main(){
 
 
 ifstream file; //our object
-
 string question; //just for the question
-
 char partAnswer[100]; //a 100 index array to fit our strings into
-
 int counter = 0; //counter
-
 bool ignoreN = false; //known as a "false flag" it will turn true later on if 
                       //correct conditions are met
 
@@ -34,57 +30,67 @@ bool ignoreN = false; //known as a "false flag" it will turn true later on if
 file.open("answers.txt");
 
 //checks to see if the file even opens
-if(file.fail())
+if(!file.fail())
 {
-    cout << "problem detected" << endl;
-    return 0;
-}
+  cout << "please  type in your question" << endl;
+            cin >> question;
 
-
-    cout << "please  type in your question" << endl;
-
-        cin >> question;
-
-while(!file.eof())  //this will allow us to read the whole line
-{
-
-    if(partAnswer[counter - 1] == '\n') //this will give us our answer
-    {   
-        cout << partAnswer << endl;
-        counter = 0;
-    }
-
-    else if(partAnswer[counter - 1] == '#') //this code checks if theres a hash in the c-string
+    while(!file.eof())  //this will allow us to read the whole line
     {
 
-       int NEXT_CHAP;
-       NEXT_CHAP = get_next_num();
+  
+            
+        
+   
 
-       if(NEXT_CHAP >= 20) 
-       {
-            partAnswer[counter - 1] = '1';
-            partAnswer[counter++] = (NEXT_CHAP - 10) + '0';
+        if(partAnswer[counter - 1] == '\n') //this will give us our answer
+        {   
+            cout << partAnswer;
+            counter = 0;
+            cout << "please  type in your question" << endl;
+            cin >> question;
+        }
 
-       }
-       else
-       {
-            partAnswer[counter - 1] = (NEXT_CHAP - 10) + '0';
-       }
-        ignoreN = true;
+
+        else if(partAnswer[counter - 1] == '#') //this code checks if theres a hash in the c-string
+        {
+
+            int NEXT_CHAP;
+            NEXT_CHAP = get_next_num();
+
+            if(NEXT_CHAP >= 10) 
+                {
+                    partAnswer[counter - 1] = '1';
+                    partAnswer[counter++] = (NEXT_CHAP - 10) + '0';
+
+                }
+            else
+                {
+                    partAnswer[counter - 1] = (NEXT_CHAP - 10) + '0';
+                }
+                ignoreN = true;
+        }
+
+        else if(ignoreN && partAnswer[counter - 1] == 'N')
+            {
+                counter--;
+                file.get(partAnswer[counter++]);
+                continue;
+            }
+
+
+     file.get(partAnswer[counter++]);
+        
     }
-
-    else if(ignoreN && partAnswer[counter - 1] == 'N'){
-        counter--;
-        file.get(partAnswer[counter++]);
-        continue;
-    }
-
-
-   file.get(partAnswer[counter++]);
-
-
-
+    
 }
 
+else
+    cout << "your filename is incorrect" << endl;
+
+
+file.close();
+
 return 0;    
+
 }
