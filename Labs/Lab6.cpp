c7731efbbcde39a4eeb1357a3d4fa48c173bd08a
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-07-06 13:15:59 -0700

    multidimensional array online

--- Labs/Lab6.cpp
+++ Labs/Lab6.cpp
@@ -2,16 +2,57 @@
 
 using namespace std;
 
+const int ROWS = 4;
+
+int COLUMNS_PER_ROWS_ARR [ROWS] = {5, 6, 4, 3};
+
 int main(){
 
 
-int user_Id = 0;
-int lab_Num = 0;
-int comp_Station = 0;
 
+int user_Id = 12345;
+int lab_Num = 3;
+int comp_Station = 2;
+
+string setter = "empty";
+
+
+
+string **arr = new string *[ROWS];
+
+arr[0] = new int [5];
+arr[1] = new int [6];
+arr[2] = new int [4];
+arr[3] = new int [3];
+
+
+
+arr[lab_Num -1][comp_Station -1] = user_Id;
+
+for(int i = 0; i < ROWS; i++){
+    for(int j = 0; j< COLUMNS_PER_ROWS_ARR[i]; j++)
+    {
+        arr[i][j] = setter;
+
+    }
+
+        cout << endl;
+}
+
+for(int i = 0; i < ROWS; i++){
+    for(int j = 0; j< COLUMNS_PER_ROWS_ARR[i]; j++)
+    {
+        cout << arr[i][j] << " ";
+
+    }
+
+        cout << endl;
+}
+
+/*
 cout << "please enter your user ID" << endl;
     cin >> user_Id;
-    if(sizeof(user_Id) > 5)
+    if(sizeof(user_Id) != 5)
     {
         cout << "you input too many numbers" << endl;
         return 0;
@@ -35,6 +76,15 @@ cout << "please enter your comp station" << endl;
         return 0;
     }
 
+*/
+
+
+
+
+delete arr[0];
+delete arr[1];
+delete arr[2];
+delete arr[3];
 
 
 
