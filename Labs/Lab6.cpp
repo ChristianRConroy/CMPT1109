4285a32c8bc7999816795fbc029d358e1f6ffdd1
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-07-06 18:16:48 -0700

    Login, Logout and error catches work
    login - works as intended
    
    Logout - now prints lab screen
    
    Error Catches - tweaked to work with functions and now just reenters while loop

--- Labs/Lab6.cpp
+++ Labs/Lab6.cpp
@@ -11,6 +11,8 @@ int COLUMNS_PER_ROWS_ARR [ROWS] = {5, 6, 4, 3};
 
 void login_Display(){
 
+    cout << "choose one of the following" << endl;
+    cout << " " << endl;
     cout << "1 - Login" << endl;
     cout << "2 - Logoff" << endl;
     cout << "3 - Search" << endl;
@@ -30,10 +32,10 @@ int get_User()
     cin >> user_Id;
         
         if(user_Id > 99999 || user_Id < 10000)
-        {
-            cout << "your user id input is invalid" << endl;
-            return 0;
-        }
+            {
+                cout << "your user id input is invalid" << endl;
+                user_Id = 0;
+            }
 
     return user_Id;
 }
@@ -43,45 +45,92 @@ int get_Lab_Num(){
    
    int lab_Num = 0;
    
-    cout << "please enter your lab number" << endl;
-    cin >> lab_Num;
+   cout << "please enter your lab number" << endl;
+
+   cin >> lab_Num;
 
     if(lab_Num > 4 || lab_Num < 0)
-    {
-        cout << "your lab number input is invalid" << endl;
+        {
+            cout << "your lab number input is invalid" << endl;
 
-        return 0;
-    }
+            lab_Num = 0;
+        }
 
-   
     return lab_Num;
 }
 
-int get_Comp_Station(){
+int get_Comp_Station(int lab_Num){
 
-int comp_Station = 0;
+    int comp_Station = 0;
 
     cout << "please enter your comp station" << endl;
+
     cin >> comp_Station;
 
-    if(comp_Station > 4 || comp_Station < 0)
+    switch(lab_Num)
     {
-        cout << "your comp station input is invalid" << endl;
-        return 0;
-    }
 
+        case 1:
+        {
+            if(comp_Station > 5 || comp_Station < 0)
+            {
+                cout << "invalid entry for comp station" << endl;
+                comp_Station = 0;
+            }
+            break;
+        }
+
+        case 2:
+        {
+            if(comp_Station > 6 || comp_Station < 0)
+            {
+                cout << "invalid entry for comp station" << endl;
+                comp_Station = 0;
+            }
+            break;
+        }
+
+        case 3:
+        {
+            if(comp_Station > 4 || comp_Station < 0)
+            {
+                cout << "invalid entry for comp station" << endl;
+                comp_Station = 0;
+            }
+            break;
+        }
+
+        case 4:
+        {
+            if(comp_Station > 3 || comp_Station < 0)
+            {
+                cout << "invalid entry for comp station" << endl;
+                comp_Station = 0;
+            }
+            break;
+        }
+
+        default:
+        {
+            cout << " invalid lab choice" << endl;
+            comp_Station = 0;
+            break;
+        }
+
+    }
 
 return comp_Station;
 
-
 }
+
+
 
 int main(){
 
 //declarations and initializations
-    int user_Id = 0, lab_Num = 0, comp_Station = 0,  selection = 0, counter = 0;
+    int user_Id = 0, lab_Num = 0, comp_Station = 0,  selection = 0, counter = 0, errorCatch = 0;
 
-    //our dynamic  pointer array
+    //our dynamic pointer array
     int **arr = new int *[ROWS];
 
     // our arrays within our pointer array
@@ -102,77 +151,117 @@ for(int i = 0; i < ROWS; i++){
 
 
 //starting of the menu
-cout << "choose one of the following" << endl;
 
-do{
 
+do
+{
+
+    //sets up our menu
     login_Display();
 
+    //grabs the selection 
     cin >> selection;
 
-//allows for choice of login, logoff, search and exit
-switch(selection){
-
-    case 1:
-        {
-                
-    user_Id = get_User();
-                
-    lab_Num = get_Lab_Num(); 
-
-    comp_Station = get_Comp_Station();
-           
-
-
-arr[lab_Num -1][comp_Station -1] = user_Id;
-
-for(int i = 0; i < ROWS; i++){
-    for(int j = 0; j< COLUMNS_PER_ROWS_ARR[i]; j++)
+    //allows for choice of login, logoff, search and exit
+    switch(selection)
     {
-        if(arr[i][j] == 0)
-            cout << "empty" << " ";
-        else
-            cout << arr[i][j] << " ";
+
+        case 1: //login
+            {
+                //gets our user id, lab number and computer number       
+                user_Id = get_User();
+
+                if(user_Id == 0)
+                   { 
+                        errorCatch = 10;
+                        break;
+                    }    
+                lab_Num = get_Lab_Num();
+
+                if(lab_Num  == 0)
+                   { 
+                        errorCatch = 10;
+                        break;
+                    }
+                comp_Station = get_Comp_Station(lab_Num);
+
+                if(comp_Station == 0)
+                   { 
+                        errorCatch = 10;
+                        break;
+                    }
+
+                
+
+                //effectively is what places our user id in the multidimensional array 
+                arr[lab_Num -1][comp_Station -1] = user_Id;
+
+                for(int i = 0; i < ROWS; i++)
+                    {
+                        for(int j = 0; j< COLUMNS_PER_ROWS_ARR[i]; j++)
+                            {
+                                if(arr[i][j] == 0) //if there are "empty" quote on quote entries, they hold empty
+                                    cout << "empty" << " ";
+                                else
+                                    cout << arr[i][j] << " "; //otherwise a value is assigned to them
+
+                            }
+                            cout << endl;
+                    }
+                    break;
+            }
+        case 2: //logout
+            {
+               // user_Id = get_User(); 
+                lab_Num = get_Lab_Num();
+                comp_Station = get_Comp_Station(lab_Num);
+
+                arr[lab_Num - 1][comp_Station - 1] = 0;
+
+                    for(int i = 0; i < ROWS; i++)
+                    {
+                        for(int j = 0; j< COLUMNS_PER_ROWS_ARR[i]; j++)
+                            {
+                                if(arr[i][j] == 0) //if there are "empty" quote on quote entries, they hold empty
+                                    cout << "empty" << " ";
+                                else
+                                    cout << arr[i][j] << " "; //otherwise a value is assigned to them
+
+                            }
+                            cout << endl;
+                    }
+                break;
+            }
+        case 3: //linear search
+            {
+
+                break;
+            }
+        case 4:
+            {   
+                //prevents memory leak in case of shutdown
+                delete arr[0];
+                delete arr[1];
+                delete arr[2];
+                delete arr[3];
+                delete arr;
+                return 0;
+                break;
+            }
+
+        default:
+            {
+                cout << "invalid entry" << endl;
+                break;
+            }
+
+
+
 
     }
 
-        cout << endl;
 }
-
-
-
-             break;
-        }
-    case 2:
-        {
-            
-            break;
-        }
-    case 3:
-        {
-
-            break;
-        }
-   case 4:
-        {
-            return 0;
-            break;
-        }
-
-    default:
-        {
-            cout << "invalid entry" << endl;
-            break;
-        }
-
-
-
-
-}
-
-}while(selection != -1);
-
-
+while(selection != -1);
 
 
 
@@ -182,8 +271,8 @@ delete arr[2];
 delete arr[3];
 delete arr;
 
-
-
-
-    return 0;
+return 0;
 }
+
+
+
