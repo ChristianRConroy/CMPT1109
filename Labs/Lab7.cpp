aacd8146651cc98cea123795678d3c92cef8e823
ChristianRConroy <106277507+ChristianRConroy@users.noreply.github.com>
2022-07-13 13:28:23 -0700

    Most functions now work
    getaverage still returns garbage

--- Labs/Lab7.cpp
+++ Labs/Lab7.cpp
@@ -12,71 +12,79 @@ string name;
 
 string rating[4];
 
+string MBAA;
+
 int numRating1, numRating2, numRating3, numRating4, numRating5;
 
 public:
 
 
-Movie (string x, string rating)
+Movie (string name, string MBAA)
 {
 
-string name = x;
-
-string rating[4] = {"G", "PG", "PG-13", "R"};
+//these connect our inputs to the members of our class
+this -> name = name; 
+this -> MBAA = MBAA;
 
 int numRating1 = 0, numRating2 = 0, numRating3 = 0, numRating4 = 0, numRating5 = 0; 
 
 }
 
-void addRating(int num){
-
-switch(num)
+Movie()
 {
 
-    case 1:
-    {	
-
-        break;
-    }
-
-    case 2:
-    {
-        
-        break;
-    }
-
-    case 3:
-    {
-        
-        break;
-    }
-
-    case 4:
-    {	
-
-        break;
-    {
-    case 5:
-    {
-
-        break;
-    }
-
-    default:
-    {
-
-        break;
-    }
 
 
-
-    }   
 }
 
-int getAverage (){
-
-    ;
+void addRating(int num)
+{
+    if(num == 1)
+        numRating1++;
+    else if(num == 2)
+        numRating2++;
+    else if(num == 3)
+        numRating3++;
+    else if(num == 4)
+        numRating4++;
+    else if(num == 5)
+        numRating5++;
+    else
+        cout << "invalid entry" << endl;
 }
+
+string getName()
+{
+    return name;
+}
+
+string getMBAA(){
+
+    return MBAA;
+}
+
+
+double getAverage (){
+
+    double total = 0, bottom = 0;
+
+    bottom = (numRating1 + numRating2 + numRating3 + numRating4 + numRating5) * 1.0;
+
+    total = (numRating1*1 + numRating2*2 + numRating3*3 + numRating4*4 + numRating5*5) / bottom;
+
+    return total;
+}
+
+void setName(string name)
+{
+    this -> name = name;
+}
+
+void setMbaa (string MBAA)
+{
+    this -> MBAA = MBAA;
+}
+
 
 
 
@@ -94,10 +102,11 @@ movie1.addRating(1);
 movie1.addRating(3);
 movie1.addRating(3);
 movie1.addRating(5);
-movie1.getName();
-movie1.getMBAA();
-cout << movie1.getAverage();
+cout << movie1.getName() << endl;
+cout << movie1.getMBAA() << endl;
+cout << movie1.getAverage() << endl;
 
+/*
 Movie movie2("The Godfather", "R");
 
 movie2.addRating(1);
@@ -110,7 +119,7 @@ movie2.getMBAA();
 cout << movie2.getAverage();
 
 
-
+*/
 
 return 0;
 
